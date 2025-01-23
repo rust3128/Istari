2ccc33e4d2aad4b81564c4b45f7621c294f2b467
rust3128 <rust3128@gmail.com>
2025-01-23 12:40:02 +0200

    Version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
+#include "version.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString versionInfo = QString("Version: %1.%2.%3\nBuild Date: %4")
+                              .arg(PROJECT_VERSION_MAJOR)
+                              .arg(PROJECT_VERSION_MINOR)
+                              .arg(PROJECT_VERSION_PATCH)
+                              .arg(PROJECT_BUILD_DATE);
+
+    qDebug() << versionInfo;
+
     MainWindow w;
     w.show();
     return a.exec();
